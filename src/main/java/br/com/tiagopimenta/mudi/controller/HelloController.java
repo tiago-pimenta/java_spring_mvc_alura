709665bab1a465568b728657d117d50ca5fa6646
tiago-pimenta <tiagopimenta@gmail.com>
2025-04-01 11:35:49 -0300

    commit

---
+++ src/main/java/br/com/tiagopimenta/mudi/controller/HelloController.java
@@ -0,0 +1,20 @@
+package br.com.tiagopimenta.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String hello(HttpServletRequest request) {
+		
+		request.setAttribute("nome", "Mundo");
+		
+		return "hello";
+		
+	}
+	
+}
