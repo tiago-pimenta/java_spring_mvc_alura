bc3b62f0537f5bf904e879fffcd64ed09af22e46
tiago-pimenta <tiagopimenta@gmail.com>
2025-04-01 12:16:13 -0300

    commit

--- src/main/java/br/com/tiagopimenta/mudi/controller/HomeController.java
+++ src/main/java/br/com/tiagopimenta/mudi/controller/HomeController.java
@@ -1,13 +1,30 @@
 package br.com.tiagopimenta.mudi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.tiagopimenta.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		
+		Pedido pedido = new Pedido();
+		
+		pedido.setNomeProduto("Xiaomi Redmi Note 8");
+		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/71JD-PdexLL._AC_SL1500_.jpg");
+		pedido.setUrlProduto("");
+		pedido.setDescricao("uma descrição qualquer para esse pedido");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		
+		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
 		
